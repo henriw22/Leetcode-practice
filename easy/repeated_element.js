fa177a9d1bff7cb9e04b60b2cc8dfa14942912ef
Henri William <83308210+henriw22@users.noreply.github.com>
2021-09-13 17:52:16 -0700

    leetcode problem #961. N-Repeated Element in Size 2N Array

---
+++ easy/repeated_element.js
@@ -0,0 +1,9 @@
+var repeatedNTimes = function(nums) {
+  for (let i = 0; i < nums.length; i++) {
+      for (let j = 0; j < nums.length; j++) {
+          if ((nums[i] === nums[j]) && (i !== j)) {
+              return nums[i];
+          }
+      }
+  }
+};
