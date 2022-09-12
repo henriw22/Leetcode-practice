313686023e2271b7cf01192c60dafbe0c15b77f5
Henri William <83308210+henriw22@users.noreply.github.com>
2022-09-11 20:11:21 -0700

    leetcode problem #2161. Partition Array According to Given Pilot

---
+++ medium/pivot_array.js
@@ -0,0 +1,15 @@
+var pivotArray = function(nums, pivot) {
+  let less = [];
+  let more = [];
+  let equal = [];
+  for (let i = 0; i < nums.length; i++) {
+      if (nums[i] < pivot) {
+          less.push(nums[i]);
+      } else if (nums[i] > pivot) {
+          more.push(nums[i]);
+      } else {
+          equal.push(nums[i]);
+      }
+  }
+  return [...less,...equal,...more];
+};
