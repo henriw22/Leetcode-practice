d37ae1ad66709f3533dd5ae79166f1f0c468f621
Henri William <83308210+henriw22@users.noreply.github.com>
2022-09-11 22:59:51 -0700

    1741. Total Time Spent by Each Employee

--- easy/leetcode_db.sql
+++ easy/leetcode_db.sql
@@ -1,2 +1,5 @@
--- 1741. Find Total Time Spent by Each Employee
+-- 1757. Recyclable and Low Fat Products
 select product_id from Products where low_fats = "Y" and recyclable = "Y";
+
+-- 1741. Find Total Time Spent by Each Employee
+select event_day as day, emp_id, sum(out_time - in_time) as total_time from Employees group by day, emp_id;
